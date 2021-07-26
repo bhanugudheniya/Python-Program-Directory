d9a81c188cc8ffd26f1fcf107cf4492b2149672e
Bhanu Gudheniya <bhanugudheniya1409@gmail.com>
2021-07-26 23:59:53 +0530

    add

---
+++ CWH_Program_Practice_DIR/CH7_Loops_ForWithBreak.py
@@ -0,0 +1,16 @@
+fruits = ['apple', 'banana', 'orange']
+
+for i in fruits:
+	print(i)
+	if i == 'banana':
+		break
+
+
+# other
+
+print('****************************')
+
+for i in fruits:
+	if i == 'banana':
+		break
+	print(i)
